12b06761cdb2efe05b75d8cf5b48daf245c5fe0c
Andrey Mattos <andreysmattos@hotmail.com>
2020-03-31 11:01:11 -0300

    add auth

--- __tests__/unit/Validator.js
+++ __tests__/unit/Validator.js
@@ -1,7 +1,7 @@
-const { EmailValidator } = require('../../src/lib/validation');
+const { EmailValidator, TokenValidator } = require('../../src/lib/validation');
 
 
-describe('Validator', () => {
+describe('EmailValidator', () => {
     it('Deve retornar FALSE ao passar um email sem @', () => {
         const email = 'any_email.com.br';
         const validate_email = EmailValidator.isValid(email);
@@ -16,6 +16,43 @@ describe('Validator', () => {
     it('Deve retornar ERRO ao não passar um email', () => {
         const email = '';
         expect( () => EmailValidator.isValid(email) ).toThrow();
+    });
+
+});
+
+describe('TokenValidator', () => {
+
+    require('dotenv').config({
+        path: process.env.NODE_ENV == 'test' ? '.env.test' : '.env'
+    });
+    
+    const TokenClass = require('../../src/lib/Token');
+
+    it('Deve retornar FALSE ao passar um token INVALIDO', () => {
+        const token = 'any_token_daskjldlakjdlaçdlaml.dasçkdaçdaskda.joasfdhdfjjsdkjfsdl';
+        const validate_token = TokenValidator.isValid(token);
+        expect(validate_token).toBe(false);
+    });
+
+    it('Deve retornar TRUE ao passar um token VALIDO', async () => {
+        const Token = new TokenClass();
+        const token = await Token.generate(12);
+        const validate_token = TokenValidator.isValid(token);
+        expect(validate_token).toBe(true);
+    });
+
+    it('Deve retornar TRUE ao passar um token VALIDO', async () => {
+        const Token = new TokenClass();
+        const token = await Token.generate(12);
+        const validate_token = TokenValidator.isValid(token);
+        expect(validate_token).toBe(true);
+    });
+
+    it('Deve retornar TRUE ao passar um token VALIDO', async () => {
+        const Token = new TokenClass();
+        const token = await Token.generate(12);
+        const validate_token = TokenValidator.isValid(token);
+        expect(validate_token).toBe(true);
     });
 
 });
--- src/app/http/middlawares/Auth.js
+++ src/app/http/middlawares/Auth.js
@@ -1,6 +1,37 @@
-module.exports = (req, res, next) =>{
+// const jwt = require('jsonwebtoken');
+const Token = require('../../../lib/Token');
+const { MissingParamError, InvalidParamError } = require('../../../lib/errors');
+const { TokenValidator } = require('../../../lib/validation');
 
-    console.log('Funciona')
+module.exports = (req, res, next) => {
+    try {
 
-    next()
+        const { authorization } = req.headers;
+
+        if (!authorization) {
+            throw new MissingParamError('Header Authorization');
+        }
+
+        const parts = authorization.split(' ');
+
+        if (parts.length !== 2) {
+            throw new InvalidParamError('Header Authorization');
+        }
+
+        const [scheme, token] = parts;
+
+        if (! /^Bearer$/i.teste(scheme)) {
+            throw new InvalidParamError('Header Authorization');
+        }
+
+        if (!TokenValidator.isValid(token)) {
+            throw new InvalidParamError('Header Authorization');
+        }
+
+        // Verificar TOKEN AKI.
+        Token.verify();
+
+    } catch (e) {
+        next(e);
+    }
 }
